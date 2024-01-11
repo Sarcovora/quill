3796b16e21ddf59bba9eccaaabb2917d20899518
Evan Kuo <evan.kuo.edu@gmail.com>
2024-01-10 22:34:19 -0600

    stripe updated

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -92,6 +92,52 @@ export const appRouter = router({
     return { url: stripeSession.url };
   }),
 
+  // createStripeSession: privateProcedure.mutation(async ({ ctx }) => {
+  //   const { userId } = ctx;
+
+  //   const billingUrl = absoluteUrl('/dashboard/billing');
+
+  //   if (!userId) throw new TRPCError({ code: 'UNAUTHORIZED' });
+
+  //   const dbUser = await db.user.findFirst({
+  //     where: {
+  //       id: userId,
+  //     },
+  //   });
+
+  //   if (!dbUser) throw new TRPCError({ code: 'UNAUTHORIZED' });
+
+  //   const subscriptionPlan = await getUserSubscriptionPlan();
+
+  //   if (subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
+  //     const stripeSession = await stripe.billingPortal.sessions.create({
+  //       customer: dbUser.stripeCustomerId,
+  //       return_url: billingUrl,
+  //     });
+
+  //     return { url: stripeSession.url };
+  //   }
+
+  //   const stripeSession = await stripe.checkout.sessions.create({
+  //     success_url: billingUrl,
+  //     cancel_url: billingUrl,
+  //     payment_method_types: ['card', 'paypal'],
+  //     mode: 'subscription',
+  //     billing_address_collection: 'auto',
+  //     line_items: [
+  //       {
+  //         price: PLANS.find((plan) => plan.name === 'Pro')?.price.priceIds.test,
+  //         quantity: 1,
+  //       },
+  //     ],
+  //     metadata: {
+  //       userId: userId,
+  //     },
+  //   });
+
+  //   return { url: stripeSession.url };
+  // }),
+
   getFileMessages: privateProcedure
     .input(
       z.object({
