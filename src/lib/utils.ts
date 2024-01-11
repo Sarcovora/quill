cb494e7c38a50cba36b6901bc83e70f62409df5c
Evan Kuo <evan.kuo.edu@gmail.com>
2024-01-10 21:14:57 -0600

    final fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path;
-  if (process.env.VERCEL_URL) return `https://$(process.env.VERCEL_URL)${path}`;
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}${path}`;
+  }
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
 
